d9b48912ee0352f81eb74a342901e3c65b325a41
Mauro Van der Flaes <mauro.vanderflaes@gmail.com>
2023-11-29 21:01:20 +0100

    Feat: Math.sign + printing into p element

--- main.js
+++ main.js
@@ -5,6 +5,11 @@ document.getElementById('sendNumber').addEventListener('click', function () {
   let number = document.getElementById('numberChooser').value;
   let result = number;
   console.log(result);
-
-
+  let check = Math.sign(result);
+  if (check !== 1) {
+    document.getElementById('result').innerHTML = "the number is negative";
+  }
+  else {
+    document.getElementById('result').innerHTML = "the number is positive";
+  }
 });
