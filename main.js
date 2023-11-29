e8a2e411292a0da5b1d78fe776f618244c9800ec
Mauro Van der Flaes <mauro.vanderflaes@gmail.com>
2023-11-29 21:14:16 +0100

    Feat: getting the value + log it + HTML JS ready

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 import './style.css'
 
 
+document.getElementById('sendNumber').addEventListener('click', function () {
+  let number = document.getElementById('numberChooser').value;
+  let result = number;
+  console.log(result);
 
+});
