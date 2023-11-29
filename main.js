94d2512f586bd2f60b4d60c9052abfbcd61ba8f3
Mauro Van der Flaes <mauro.vanderflaes@gmail.com>
2023-11-29 21:12:34 +0100

    first commit

--- main.js
+++ main.js
@@ -1,15 +1,4 @@
 import './style.css'
 
 
-document.getElementById('sendNumber').addEventListener('click', function () {
-  let number = document.getElementById('numberChooser').value;
-  let result = number;
-  console.log(result);
-  let check = Math.sign(result);
-  if (check !== 1) {
-    document.getElementById('result').innerHTML = "the number is negative";
-  }
-  else {
-    document.getElementById('result').innerHTML = "the number is positive";
-  }
-});
+
